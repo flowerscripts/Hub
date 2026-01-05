971619eea3a686b8f86937fac067217ef889467d
jacob <luaujacob@gmail.com>
2026-01-04 23:15:33 -0700

    Update Permafall.lua

--- files/games/Permafall.lua
+++ files/games/Permafall.lua
@@ -1412,7 +1412,7 @@ do -- // ESP Functions
         local npcName = npc.Name;
 
         local PurchaseInfo = FindFirstChild(npc, 'PurchaseInfo');
-        if (PurchaseInfo) then npcName = PurchaseInfo.ItemName .. ' [Purchasable]'
+        if (PurchaseInfo) then npcName = PurchaseInfo.ItemName .. ' [Purchasable]' end;
         
         local npcObj;
         if (npc:IsA('BasePart') or npc:IsA('MeshPart')) then
